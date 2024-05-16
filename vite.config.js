2596dc48d0a3f509dd9842b52a12b82ed9c6c39d
srappltd <abhaygautam626039@gmail.com>
2024-05-16 17:14:09 +0530

    last last last

--- vite.config.js
+++ vite.config.js
@@ -3,47 +3,47 @@ import react from '@vitejs/plugin-react'
 // import { VitePWA } from 'vite-plugin-pwa';
 // import VitePWA from './src/Utils/WebAppSetupPWA'
 // import { generateSW } from 'workbox-build';
-import {VitePWA} from 'vite-plugin-pwa'
+// import {VitePWA} from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType:"autoUpdate",
-  includeAssets:['favicon.ico', 'apple-touch-icon.png', 'https://blinkit.com/48167d399a1881b810d7.png'],
-  manifest:{
-    name:"Blinkit Shoping",
-    short_name:"Blinkit",
-    theme_color:"#ffffff",
-    orientation:"portrait",
-    icons:[
-      {
-        src: 'https://blinkit.com/48167d399a1881b810d7.png',
-        sizes: '64x64',
-        type: 'image/png'
-      },
-      {
-        src: 'https://blinkit.com/48167d399a1881b810d7.png',
-        sizes: '192x192',
-        type: 'image/png'
-      },
-      {
-        src: 'https://blinkit.com/48167d399a1881b810d7.png',
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'any'
-      },
-      {
-        src: 'https://blinkit.com/48167d399a1881b810d7.png',
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'maskable'
-      },
+  //   VitePWA({
+  //     registerType:"autoUpdate",
+  // includeAssets:['favicon.ico', 'apple-touch-icon.png', 'https://blinkit.com/48167d399a1881b810d7.png'],
+  // manifest:{
+  //   name:"Blinkit Shoping",
+  //   short_name:"Blinkit",
+  //   theme_color:"#ffffff",
+  //   orientation:"portrait",
+  //   icons:[
+  //     {
+  //       src: 'https://blinkit.com/48167d399a1881b810d7.png',
+  //       sizes: '64x64',
+  //       type: 'image/png'
+  //     },
+  //     {
+  //       src: 'https://blinkit.com/48167d399a1881b810d7.png',
+  //       sizes: '192x192',
+  //       type: 'image/png'
+  //     },
+  //     {
+  //       src: 'https://blinkit.com/48167d399a1881b810d7.png',
+  //       sizes: '512x512',
+  //       type: 'image/png',
+  //       purpose: 'any'
+  //     },
+  //     {
+  //       src: 'https://blinkit.com/48167d399a1881b810d7.png',
+  //       sizes: '512x512',
+  //       type: 'image/png',
+  //       purpose: 'maskable'
+  //     },
       
-    ]
-  }
-    })
+  //   ]
+  // }
+  //   })
 
   ],
 })
